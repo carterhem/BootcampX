71c504f043c98ce8a2f719fc976aedb29985f2eb
Carter Hemington <carter.hemington@gmail.com>
2022-02-08 15:12:50 +0000

    5 working - had a spelling error and the order of JOINS wrong

--- 4_queries/5_average_cohort_assistance_time.sql
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -1,14 +1,6 @@
 SELECT cohorts.name AS name, AVG(completed_at - started_at) AS average_assistance_time
 FROM assistance_requests
-JOIN cohorts ON name = cohorts.name
-JOIN students ON student_id = students.id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
-ORDER BY average_assistance_time ASC;
-
-
-SELECT cohorts.name AS name, AVG(completed_at - started_at) AS average_assistance_time
-FROM assistance_requests
-JOIN cohorts ON name = cohorts.name
-JOIN students ON student_id = students.id
-GROUP BY cohorts.name
-ORDER BY average_assistance_time ASC;
+ORDER BY average_assistance_time;
